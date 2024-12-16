5cc83bb1b95a477e216a1c790c833e1719aa6415
BQuent1 <quentin.baud9@gmail.com>
2024-12-16 11:44:56 +0100

    style et mise en page

--- src/html/js/script_result.js
+++ src/html/js/script_result.js
@@ -1,6 +1,6 @@
 const queryParams = new URLSearchParams(window.location.search);
 
-document.getElementById("recap_genre").textContent = queryParams.get("genre") || "Non spécifié";
+document.getElementById("recap_genre").textContent = queryParams.get("genre") || "Non renseigné";
 document.getElementById("recap_nom").textContent = queryParams.get("nom") || "Non renseigné";
 document.getElementById("recap_prenom").textContent = queryParams.get("prenom") || "Non renseigné";
 document.getElementById("recap_email").textContent = queryParams.get("email") || "Non renseigné";
@@ -8,3 +8,4 @@ document.getElementById("recap_telephone").textContent = queryParams.get("tel") 
 document.getElementById("recap_adresse").textContent = queryParams.get("adresse") || "Non renseignée";
 document.getElementById("recap_cp").textContent = queryParams.get("CP") || "Non renseigné";
 document.getElementById("recap_ville").textContent = queryParams.get("ville") || "Non renseignée";
+document.getElementById("recap_quantite").textContent = queryParams.get("quantite") || "Non spécifiée";
