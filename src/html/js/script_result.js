9c7bc27817b882c7d7a632e6bb53abe6f3f22a07
BQuent1 <quentin.baud9@gmail.com>
2024-12-16 11:04:50 +0100

    création de deux pages manquantes

---
+++ src/html/js/script_result.js
@@ -0,0 +1,10 @@
+const queryParams = new URLSearchParams(window.location.search);
+
+document.getElementById("recap_genre").textContent = queryParams.get("genre") || "Non spécifié";
+document.getElementById("recap_nom").textContent = queryParams.get("nom") || "Non renseigné";
+document.getElementById("recap_prenom").textContent = queryParams.get("prenom") || "Non renseigné";
+document.getElementById("recap_email").textContent = queryParams.get("email") || "Non renseigné";
+document.getElementById("recap_telephone").textContent = queryParams.get("tel") || "Non renseigné";
+document.getElementById("recap_adresse").textContent = queryParams.get("adresse") || "Non renseignée";
+document.getElementById("recap_cp").textContent = queryParams.get("CP") || "Non renseigné";
+document.getElementById("recap_ville").textContent = queryParams.get("ville") || "Non renseignée";
